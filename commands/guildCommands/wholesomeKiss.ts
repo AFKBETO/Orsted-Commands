73cb46379fbb5b2bf7147eeedd5743442e07aab2
afkbeto <qv.nguyen00@gmail.com>
2025-02-03 18:38:20 +0100

    feat: add wholesomeKiss

---
+++ commands/guildCommands/wholesomeKiss.ts
@@ -0,0 +1,24 @@
+import { BotCommand } from '@orsted/utils';
+import { SlashCommandBuilder } from 'discord.js';
+import { setCommandName } from '../../utils/setCommandName.ts';
+
+const wholesomeUrl = [
+    'https://media.discordapp.net/attachments/824175906120663060/843483983080849418/E0ndCMxVoAAeSy3.png?width=804&height=504',
+    'https://media.discordapp.net/attachments/824175906120663060/843465128644313108/20210511_104844.png?width=952&height=504',
+];
+
+const wholesomeKiss: BotCommand = {
+    data: new SlashCommandBuilder()
+        .setName(setCommandName('wholesomekiss'))
+        .setDescription('Post a wholesome kiss'),
+    execute: async (interaction) => {
+        try {
+            await interaction.reply(wholesomeUrl.randomItem());
+        } catch (error) {
+            console.error(new Date(), 'wholesomeKiss');
+            console.error(error);
+        }
+    },
+};
+
+export default wholesomeKiss;
