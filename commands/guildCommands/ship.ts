02227d6b9dd3a69f3bee37addb4483a2f1748439
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-03-20 21:39:44 +0100

    feat: add ship command

---
+++ commands/guildCommands/ship.ts
@@ -0,0 +1,63 @@
+import {
+    CommandInteraction,
+    CommandInteractionOptionResolver,
+    SlashCommandBuilder,
+} from 'discord.js';
+import { SlashCommand } from '@orsted/utils';
+import { setCommandName } from '../../utils/setCommandName.ts';
+import { randomName } from '../../utils/randomName.ts';
+
+const ship: SlashCommand = {
+    data: new SlashCommandBuilder()
+        .setName(setCommandName('ship'))
+        .setDescription('Ship somebody with someone else')
+        .addUserOption((option) =>
+            option
+                .setName('target1')
+                .setDescription('Select a user to ship')
+                .setRequired(true)
+        )
+        .addUserOption((option) =>
+            option
+                .setName('target2')
+                .setDescription('Select a second user to ship')
+        ) as SlashCommandBuilder,
+    execute: async (interaction: CommandInteraction) => {
+        try {
+            await interaction.deferReply();
+            const options = interaction
+                .options as CommandInteractionOptionResolver;
+
+            let target1 = options.getUser('target1', true);
+            const target2 = options.getUser('target2');
+            let shippedTarget = '';
+            if (
+                target1.id === interaction.client.user.id ||
+                (target2 && target2.id === interaction.client.user.id)
+            ) {
+                shippedTarget = randomName('male');
+                target1 = interaction.user;
+                await interaction.editReply(
+                    `${interaction.client.user} has shipped ${target1} with ${shippedTarget} for trying to ship with ${interaction.client.user.displayName}!`,
+                );
+                return;
+            }
+            if (!target2 || target2.id === target1.id) {
+                shippedTarget = randomName();
+                await interaction.editReply(
+                    `${interaction.user} has shipped ${target1} with ${shippedTarget}!`,
+                );
+                return;
+            }
+            await interaction.editReply(
+                `${interaction.user} has shipped ${target1} with ${target2}!`,
+            );
+            return;
+        } catch (error) {
+            console.error(new Date(), 'ship');
+            console.error(error);
+        }
+    },
+};
+
+export default ship;
